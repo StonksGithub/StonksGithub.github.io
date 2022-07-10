74a65a78ed3fbdbc8909421fec3abe044ecd4425
StonksGithub <stonks.github.cs@gmail.com>
2022-07-09 21:45:21 -0300

    Initial Commit.
    Version 0.0.1, probably is buggy

---
+++ index.js
@@ -0,0 +1,27 @@
+let dropCoins = 0
+let dropCoinsPerClick = 1
+let obstacleLevels = 0
+
+window.onload = function () {
+    const dropAmt = document.getElementById('dropAmt')
+    const obstText = document.getElementById('obstacles')
+    const obstPriceText = document.getElementById('obstaclePrice')
+}
+
+window.setInterval(function () {
+    dropAmt.innerHTML = "Players Dropped: " + droppedKids
+    obstText.innerHTML = "Totally safe! Obstacles added: " + legvels
+    obstPriceText.innerHTML = "DropCoins needed: " + legPrice
+    obstPrice = Math.pow(2, obstacleLevels + 1)
+}, 100)
+
+function dropPlayer() {
+    droppedKids += droppedPerClick
+}
+
+function aObstacle() {
+    if (dropCoins >= obstPrice) {
+        dropCoins -= obstPrice
+        obstacleLevels++
+    }
+}
