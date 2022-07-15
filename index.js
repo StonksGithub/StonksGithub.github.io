0af34214c92d60b9ad0f8b0c4a9ee3509fb5b043
StonksGithub <stonks.github.cs@gmail.com>
2022-07-15 11:50:32 -0300

    Move functions...

--- index.js
+++ index.js
@@ -1,45 +1,5 @@
 import { Decimal } from "./break_infinity.js"
 
-let dropCoins = getCookie("dropCoins") === "" ? new Decimal(0) : new Decimal(roughScale(getCookie("dropCoins"))) // if cookie exists, read it, else 0
-let dropCoinsPerClick = new Decimal(1)
-let obstacleLevels = getCookie("obstacleLvl") === "" ? new Decimal(0) : new Decimal(roughScale(getCookie("obstacleLvl"))) // if cookie exists, read it, else 0
-var obstPrice = Decimal.pow(2, (obstacleLevels + 1)) // exponential function 2^(x+1) or e^(ln2*(x+1)) for calc students
-
-var dropAmt = document.getElementById("dropAmt")
-var obstText = document.getElementById("obstacles")
-var obstPriceText = document.getElementById("obstaclePrice")
-var btn = document.getElementById("btn")
-var obstBtn = document.getElementById("aObstacle")
-
-const d = new Date();
-
-function dropPlayer() {
-    dropCoins += dropCoinsPerClick
-}
-
-function aObstacle() {
-    if (dropCoins >= obstPrice) {
-        dropCoins -= obstPrice
-        Decimal.add(obstacleLevels, 1)
-        obstPrice = Decimal.pow(2, (obstacleLevels + 1))
-    }
-}
-
-btn.addEventListener("click", dropPlayer())
-obstBtn.addEventListener("click", aObstacle())
-
-window.setTimeout(function () {
-    checkIfNOU(dropAmt, "DropCoins:", "", dropCoins)
-    checkIfNOU(obstText, "Totally safe! Obstacles added:", "lvls", obstacleLevels)
-    checkIfNOU(obstPriceText, "DropCoins needed:", "$", obstPrice)
-
-    // Save Cookie
-    d.setTime(d.getTime() + 31556926000);
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = "dropCoins=" + dropCoins + "; obstacleLvl=" + obstacleLevels + ";" + expires + "; path=/";
-}, 100)
-
-
 function getCookie(cname) { // THANK YOU W3SCHOOLS!
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
@@ -62,9 +22,47 @@ function roughScale(x, base = 10) {
     return parsed;
 }
 
+let dropCoins = getCookie("dropCoins") === "" ? new Decimal(0) : new Decimal(roughScale(getCookie("dropCoins"))) // if cookie exists, read it, else 0
+let dropCoinsPerClick = new Decimal(1)
+let obstacleLevels = getCookie("obstacleLvl") === "" ? new Decimal(0) : new Decimal(roughScale(getCookie("obstacleLvl"))) // if cookie exists, read it, else 0
+var obstPrice = Decimal.pow(2, (obstacleLevels + 1)) // exponential function 2^(x+1) or e^(ln2*(x+1)) for calc students
+
+var dropAmt = document.getElementById("dropAmt")
+var obstText = document.getElementById("obstacles")
+var obstPriceText = document.getElementById("obstaclePrice")
+var btn = document.getElementById("btn")
+var obstBtn = document.getElementById("aObstacle")
+
+const d = new Date();
+
+function dropPlayer() {
+    dropCoins += dropCoinsPerClick
+}
+    
+    function aObstacle() {
+        if (dropCoins >= obstPrice) {
+        dropCoins -= obstPrice
+        Decimal.add(obstacleLevels, 1)
+        obstPrice = Decimal.pow(2, (obstacleLevels + 1))
+    }
+}
 
 function checkIfNOU(variable, prefix = "", suffix = "", value = Decimal(0)) {
-    if(variable) {
+    if (variable) {
         variable.innerHTML = `${prefix} ${value} ${suffix}`
     }
 }
+
+btn.addEventListener("click", dropPlayer())
+obstBtn.addEventListener("click", aObstacle())
+
+window.setTimeout(function () {
+    checkIfNOU(dropAmt, "DropCoins:", "", dropCoins)
+    checkIfNOU(obstText, "Totally safe! Obstacles added:", "lvls", obstacleLevels)
+    checkIfNOU(obstPriceText, "DropCoins needed:", "$", obstPrice)
+
+    // Save Cookie
+    d.setTime(d.getTime() + 31556926000);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = "dropCoins=" + dropCoins + "; obstacleLvl=" + obstacleLevels + ";" + expires + "; path=/";
+}, 100)
